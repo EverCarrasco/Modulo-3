688e17572f409a29e1828c82c0950957d85003f9
ever.carrasco1405@gmail.com <>
2019-11-12 23:44:28 -0600

    Modulo 2

---
+++ TareaPractica_2_Ever_Carrasco/src/tareapractica/pkg2_ever_carrasco/TareaPractica2_Ever_Carrasco.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareapractica.pkg2_ever_carrasco;
+
+/**
+ *
+ * 
+ */
+public class TareaPractica2_Ever_Carrasco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Recursos r=new Recursos();
+          System.out.println("");
+        System.out.println("Primer metodo");
+      
+        r.Saludo();
+        System.out.println("Segundo metodo");
+         System.out.println("");
+        r.IngresoCalificacion();
+         System.out.println("");
+        System.out.println("Tercer metodo");
+       
+        r.Division();
+          System.out.println("");
+        System.out.println("Cuarto metodo");
+      
+        r.ListaNumeros();
+         System.out.println("");
+        System.out.println("Quinto metodo");
+       
+        r.OperacionesAritmeticas();
+    }
+    
+}
